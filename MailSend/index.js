7ec193a465af89706620b3076306a19b2fe12ef5
ChovatiyaViral <vchovatiya992@gmail.com>
2022-06-29 16:48:48 +0530

    email send from other app create

--- MailSend/index.js
+++ MailSend/index.js
@@ -2,25 +2,20 @@ const nodemailer = require('nodemailer');
 require('dotenv').config();
 
 const transporter = nodemailer.createTransport({
-    // service: "gmail",
+    service: "gmail",
     host: process.env.EMAIL_HOST,
     port: process.env.EMAIL_PORT,
-    secure: true,
-    // secure: false,
     auth: {
         user: process.env.NOTIFICATION_EMAIL,
         pass: process.env.NOTIFICATION_EMAIL_PASSWORD
     },
-    // tls: {
-    //     rejectUnauthorized: false
-    // }
 });
 
 var mailOptions = {
     from: process.env.NOTIFICATION_EMAIL,
     to: process.env.SEND_EMAIL,
-    subject: 'Subject',
-    html: "Mail content here."
+    subject: 'Welcome to my word',
+    text: "you are the hack now.  are you student now "
 };
 
 async function sendMail(ToAddresses, CcAddresses, Subject, mailContent) {
@@ -33,6 +28,13 @@ async function sendMail(ToAddresses, CcAddresses, Subject, mailContent) {
         }
     })
 }
+
+// Go to your Google account at https://myaccount.google.com/
+// Go to Security
+// In "Signing in to Google" section choose 2-Step Verification - here you have to verify yourself, in my case it was with phone number and a confirmation code send as text message. After that you will be able to enabled 2-Step Verification
+// Back to Security in "Signing in to Google" section choose App passwords
+// From the Select app drop down choose Other (Custom name) and put a name e.g. nodemailer
+// A modal dialog will appear with the password. Get that password and use it in your code.
 
 module.exports = {
     sendMail
